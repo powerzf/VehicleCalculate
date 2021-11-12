ded5ecd90ee7e38cba33c4f11a4d0586f0781d5b
powerzf <18883350900@163.com>
2021-11-12 09:56:48 +0800

    单独包

--- src/VehicleFunction.jl
+++ src/VehicleFunction.jl
@@ -1,5 +1,4 @@
 #初级传动比
-using GLMakie
 function primGear_ratio(transm::PrimTransmission)
     drivenGear = transm.drivenGear  #离合器主动齿
     drivingGear = transm.drivingGear  #离合器从动齿
@@ -82,73 +81,4 @@ function vehiclespeed(enginename::String,ratio::Transmission; ca_speed = 7500, c
     return VehicleSpeed(enginename,primgearRatio,[gear_1_ratio,gear_2_ratio,gear_3_ratio,gear_4_ratio,gear_5_ratio,gear_6_ratio],thirdgearRatio,circumfeience,ca_speed)
 
     
-end
-
-function tableplot(h,v,c;textsi = (20,16,15),reso = (600,600))
-    hlength = length(h)#行数
-    vlength = length(v)#列数
-    clength = length(c)
-
-
-
-    fg = Figure(resolution = reso)
-    axis = Axis(fg[1,1],
-
-    #aspect = reso[2]/reso[1],
-    xgridvisible = false,
-    ygridvisible = false,
-    xticklabelsvisible = false,#轴上的标记文字的显示
-    yticklabelsvisible = false,
-    xticksvisible = false,
-    yticksvisible = false,
-    # bottomspinecolor = :red,
-    # leftspinecolor = :green,
-    spinewidth = 2.0,
-    # tellheight = true,
-    # tellwidth = true,
-    #width = 400,
-    # height = 100,
-    #bottomspinevisible = false,
-    halign = :left,
-
-    limits = (0.5,vlength+1.5,0.5,hlength+1.5)
-
-    )
-
-    #hidedecorations!(axis)
-
-    #th = Attributes(xgridvisible = false,ygridvisible = true,xticks = LinearTicks.(n),)
-    xb = range(2,hlength,step = 2)
-    yb = repeat([vlength+1.5],length(xb))
-
-
-    #内容栏
-    barplot!(axis,xb,yb,
-    fillto =repeat([1.5],length(xb)),
-    width = 1,
-    direction=:x,
-    color = :lightblue)
-
-    #左边竖标题栏
-    barplot!(axis,(1,hlength+0.5),width = 1,color = RGBAf0(0.7118467f0,0.8204677f0,0.12016797f0,0.1363885f0))
-    #上面横标题栏
-    barplot!(axis,(hlength+1,vlength+1.5),width = 1,direction = :x,color = RGBAf0(0.7118467f0,0.8204677f0,0.12016797f0,0.1363885f0))
-
-    n = collect(1.5:hlength+0.5)
-    m = collect(1.5:vlength+0.5)
-    #lines!(axis, n, n)
-    vlines!(axis,m,color = :black)
-    hlines!(axis,n,color = :black)
-    #set_theme!(;th)
-    texnumbr = reverse(h)
-    ytex = range(1,hlength,step = 1)
-    text!(axis,texnumbr, position = Point.(1, ytex), align = (:center, :center),color = :black)
-    text!(axis,v,textsize=textsi[2], position = Point.(2:vlength+1, hlength+1), align = (:center, :center),color = :black)  
-    neirong = string.(reverse(round.(c;digits = 4),dims =1))
-    si = size(neirong)
-    for i in 1:si[2]
-        text!(axis,neirong[:,i], position = Point.(i+1, ytex), align = (:center, :center),color = :black)
-    end
-
-    fg
 end
